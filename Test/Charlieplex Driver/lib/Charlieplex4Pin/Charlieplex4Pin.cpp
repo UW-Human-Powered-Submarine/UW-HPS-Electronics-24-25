671b84ee642ad2b42ff5c12bea58f430bdab58e1
Zihui Liu <salzhx@gmail.com>
2025-03-12 16:44:36 -0700

    Finished class, not tested

--- Test/Charlieplex Driver/lib/Charlieplex4Pin/Charlieplex4Pin.cpp
+++ Test/Charlieplex Driver/lib/Charlieplex4Pin/Charlieplex4Pin.cpp
@@ -1,5 +1,9 @@
 #include "Charlieplex4Pin.h"
 
+//  +-------------------------------------------------------------------------+
+//  |                      Charliplex 4 Pin Driver Class                      |
+//  +-------------------------------------------------------------------------+
+
 Charlieplex4Pin::Charlieplex4Pin(int pin0, int pin1, int pin2, int pin3)
     : Charlieplex4Pin(pin0, pin1, pin2, pin3, 2000) {
 }
@@ -26,6 +30,10 @@ void Charlieplex4Pin::set_led_on_delay(unsigned long microsecond) {
     this->led_on_delay_us = microsecond;
 }
 
+unsigned long Charlieplex4Pin::get_led_on_delay_us() const {
+    return this->led_on_delay_us;
+}
+
 void Charlieplex4Pin::set_led_state(int index_permutation, bool state) {
     this->led_states[index_permutation] = state;
 }
@@ -38,12 +46,24 @@ void Charlieplex4Pin::set_green_led(int index, bool state) {
     this->led_states[CHARLIEPLEX_LED_GREEN[index]] = state;
 }
 
+bool Charlieplex4Pin::get_green_led(int index) const {
+    return this->led_states[CHARLIEPLEX_LED_GREEN[index]];
+}
+
 void Charlieplex4Pin::set_red_led(int index, bool state) {
     this->led_states[CHARLIEPLEX_LED_RED[index]] = state;
 }
 
+bool Charlieplex4Pin::get_red_led(int index) const {
+    return this->led_states[CHARLIEPLEX_LED_RED[index]];
+}
+
 void Charlieplex4Pin::set_yellow_led(int index, bool state) {
     this->led_states[CHARLIEPLEX_LED_YELLOW[index]] = state;
+}
+
+bool Charlieplex4Pin::get_yellow_led(int index) const {
+    return this->led_states[CHARLIEPLEX_LED_YELLOW[index]];
 }
 
 void Charlieplex4Pin::clear_states() {
@@ -88,6 +108,10 @@ void Charlieplex4Pin::event() {
     }
 }
 
+//  +-------------------------------------------------------------------------+
+//  |            Charliplex 4 Pin Driver with Blink Function Class            |
+//  +-------------------------------------------------------------------------+
+
 Charlieplex4PinBlink::Charlieplex4PinBlink(int pin0, int pin1, int pin2, int pin3)
     : Charlieplex4PinBlink(pin0, pin1, pin2, pin3, 250, 500) {
 }
@@ -97,7 +121,13 @@ Charlieplex4PinBlink::Charlieplex4PinBlink(
     unsigned long slow_blink_period_ms, unsigned long fast_blink_period_ms
 )
     : Charlieplex4Pin(pin0, pin1, pin2, pin3)
-    , slow_blink_period_ms(slow_blink_period_ms), fast_blink_period_ms(fast_blink_period_ms) {
+    , slow_blink_period_ms(slow_blink_period_ms), fast_blink_period_ms(fast_blink_period_ms)
+    , slow_blink_resume_time_ms(0), slow_blink_current_status(false)
+    , fast_blink_resume_time_ms(0), fast_blink_current_status(false) {
+
+    for (int i = 0; i < 12; i++) {
+        this->led_blink_states[i] = CharlieplexBlinkStates::CBS_OFF;
+    }
 }
 
 Charlieplex4PinBlink::Charlieplex4PinBlink(
@@ -107,7 +137,85 @@ Charlieplex4PinBlink::Charlieplex4PinBlink(
 )
     : Charlieplex4Pin(pin0, pin1, pin2, pin3, refresh_period_microsecond)
     , slow_blink_period_ms(slow_blink_period_ms), fast_blink_period_ms(fast_blink_period_ms) {
+
+    for (int i = 0; i < 12; i++) {
+            this->led_blink_states[i] = CharlieplexBlinkStates::CBS_OFF;
+        }
 }
 
 Charlieplex4PinBlink::~Charlieplex4PinBlink() {
+}
+
+void Charlieplex4PinBlink::set_green_led(int index, CharlieplexBlinkStates state_blink) {
+    this->led_blink_states[CHARLIEPLEX_LED_GREEN[index]] = state_blink;
+}
+
+CharlieplexBlinkStates Charlieplex4PinBlink::get_green_led(int index) const {
+    return this->led_blink_states[CHARLIEPLEX_LED_GREEN[index]];
+}
+
+void Charlieplex4PinBlink::set_red_led(int index, CharlieplexBlinkStates state_blink) {
+    this->led_blink_states[CHARLIEPLEX_LED_RED[index]] = state_blink;
+}
+
+CharlieplexBlinkStates Charlieplex4PinBlink::get_red_led(int index) const {
+    return this->led_blink_states[CHARLIEPLEX_LED_RED[index]];
+}
+
+void Charlieplex4PinBlink::set_yellow_led(int index, CharlieplexBlinkStates state_blink) {
+    this->led_blink_states[CHARLIEPLEX_LED_YELLOW[index]] = state_blink;
+}
+
+CharlieplexBlinkStates Charlieplex4PinBlink::get_yellow_led(int index) const {
+    return this->led_blink_states[CHARLIEPLEX_LED_YELLOW[index]];
+}
+
+void Charlieplex4PinBlink::set_slow_blink_period_ms(unsigned long milliseconds) {
+    this->slow_blink_period_ms = milliseconds;
+}
+
+unsigned long Charlieplex4PinBlink::get_slow_blink_period_ms() const {
+    return this->slow_blink_period_ms;
+}
+
+void Charlieplex4PinBlink::set_fast_blink_period_ms(unsigned long milliseconds) {
+    this->fast_blink_period_ms = milliseconds;
+}
+
+unsigned long Charlieplex4PinBlink::get_fast_blink_period_ms() const {
+    return this->fast_blink_period_ms;
+}
+
+void Charlieplex4PinBlink::event() {
+    unsigned long current_time = millis();
+    if (this->slow_blink_resume_time_ms <= current_time) {
+        slow_blink_update();
+        this->slow_blink_resume_time_ms = current_time + this->slow_blink_period_ms;
+    }
+
+    if (this->fast_blink_resume_time_ms <= current_time) {
+        fast_blink_update();
+        this->fast_blink_resume_time_ms = current_time = this->fast_blink_period_ms;
+    }
+
+    //  Calling parent's method to update the lower level charlieplex function
+    Charlieplex4Pin::event();
+}
+
+void Charlieplex4PinBlink::slow_blink_update() {
+    this->slow_blink_current_status = !this->slow_blink_current_status;
+    for (int i = 0; i < 12; i++) {
+        if (this->led_blink_states[i] == CharlieplexBlinkStates::CBS_BLINK_SLOW) {
+            Charlieplex4Pin::set_led_state(i, this->slow_blink_current_status);
+        }
+    }
+}
+
+void Charlieplex4PinBlink::fast_blink_update() {
+    this->fast_blink_current_status = !this->fast_blink_current_status;
+    for (int i = 0; i < 12; i++) {
+        if (this->led_blink_states[i] == CharlieplexBlinkStates::CBS_BLINK_FAST) {
+            Charlieplex4Pin::set_led_state(i, this->fast_blink_current_status);
+        }
+    }
 }
--- Test/Charlieplex Driver/lib/Charlieplex4Pin/Charlieplex4Pin.h
+++ Test/Charlieplex Driver/lib/Charlieplex4Pin/Charlieplex4Pin.h
@@ -12,7 +12,7 @@
 //  |       otherwise the display will be too dim                             |
 //  |                                                                         |
 //  |   Uncomment "#define USE_HANDSOLDERED_BOARD_CONFIG" for handsoldered    |
-//  |       board
+//  |       board                                                             |
 //  |                                                                         |
 //  |   Author     :    Zihui(Andy) Liu <liuzihui@uw.edu>                     |
 //  |   Last Update:    February 26, 2025                                     |
@@ -40,6 +40,10 @@ const int CHARLIEPLEX_LED_YELLOW[5] = {10, 7, 3, 6, 9};
 const int CHARLIEPLEX_LED_RED[5] = {5, 4, 0, 1, 2};
 #endif  //  USE_HANDSOLDERED_BOARD_CONFIG
 
+//  +-------------------------------------------------------------------------+
+//  |                      Charliplex 4 Pin Driver Class                      |
+//  +-------------------------------------------------------------------------+
+
 class Charlieplex4Pin: public Scheduler {
 public:
     Charlieplex4Pin(int pin0, int pin1, int pin2, int pin3);
@@ -47,6 +51,7 @@ public:
     ~Charlieplex4Pin();
 
     void set_led_on_delay(unsigned long microsecond);
+    unsigned long get_led_on_delay_us() const;
 
     //  Set led state
     //  index from permutation
@@ -54,8 +59,11 @@ public:
     bool get_led_state(int index_permutation) const;
 
     void set_green_led(int index, bool state);
+    bool get_green_led(int index) const;
     void set_red_led(int index, bool state);
+    bool get_red_led(int index) const;
     void set_yellow_led(int index, bool state);
+    bool get_yellow_led(int index) const;
 
     //  Set all states to 0
     void clear_states();
@@ -71,10 +79,17 @@ protected:
     void event() override;
 };
 
+//  +-------------------------------------------------------------------------+
+//  |            Charliplex 4 Pin Driver with Blink Function Class            |
+//  +-------------------------------------------------------------------------+
 
-//  The driver with blinking function
+enum CharlieplexBlinkStates {
+    CBS_OFF, CBS_ON, CBS_BLINK_SLOW, CBS_BLINK_FAST
+};
+
 class Charlieplex4PinBlink: public Charlieplex4Pin {
 public:
+
     Charlieplex4PinBlink(int pin0, int pin1, int pin2, int pin3);
     Charlieplex4PinBlink(
         int pin0, int pin1, int pin2, int pin3, 
@@ -86,9 +101,36 @@ public:
         unsigned long slow_blink_period_ms, unsigned long fast_blink_period_ms
     );
     ~Charlieplex4PinBlink();
+
+    void set_green_led(int index, CharlieplexBlinkStates state_blink);
+    CharlieplexBlinkStates get_green_led(int index) const;
+    void set_red_led(int index, CharlieplexBlinkStates state_blink);
+    CharlieplexBlinkStates get_red_led(int index) const;
+    void set_yellow_led(int index, CharlieplexBlinkStates state_blink);
+    CharlieplexBlinkStates get_yellow_led(int index) const;
+
+    void set_slow_blink_period_ms(unsigned long milliseconds); 
+    unsigned long get_slow_blink_period_ms() const;
+    void set_fast_blink_period_ms(unsigned long milliseconds); 
+    unsigned long get_fast_blink_period_ms() const;
+
+
 private:
     unsigned long slow_blink_period_ms;
     unsigned long fast_blink_period_ms;
+
+    unsigned long slow_blink_resume_time_ms;
+    bool slow_blink_current_status;
+
+    unsigned long fast_blink_resume_time_ms;
+    bool fast_blink_current_status;
+
+    CharlieplexBlinkStates led_blink_states[12];
+
+    void event() override;
+
+    void slow_blink_update();
+    void fast_blink_update();
 };
 
 #endif  //  ___CHARLIEPLEX_H___
